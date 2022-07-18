d5052965456b6806b98b3e74d857f8fc33ec2653
Jakub Cisowski <jakub.cisowski1@gmail.com>
2022-07-18 13:52:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Favourite Channels
-// @description  Ability to add twitch channels to your favourites
-// @version      0.1
+// @description  Add twitch channels to your favourites. While on Twitch: [Inspect element -> Application tab -> On the left: (Storage -> LocalStorage -> twitch.tv) -> Filter 'favourites' -> Input channel names in value tab separated by commas]
+// @version      1.1
 // @author       Jakub Cisowski
 // @include      http://www.twitch.tv/*
 // @include      https://www.twitch.tv/*
@@ -12,8 +12,30 @@
     'use strict';
 
     // Waiting 1000ms for page to load properly
-    window.setTimeout(updateFavourites, 1000, true);
-    appendSettingsButton();
+    let intervalID = window.setInterval(styleFavourites, 500);
+    // appendSettingsButton();
+
+    // Styling favourite channels
+    function styleFavourites(){
+        let followedChannels = document.querySelectorAll("[data-a-target='side-nav-title']");
+        let favouritesStrings = window.localStorage.getItem('favourites').toLowerCase();
+        let favouritesArray = favouritesStrings.split(',');
+
+        // Check if section is found (user can be switched to broadcast tab).
+        if (followedChannels.length > 0){
+            for(let i=0; i<followedChannels.length; i++){
+                if(favouritesArray.includes(followedChannels[i].title.toLowerCase())){
+                    followedChannels[i].setAttribute("style", "color: gold!important;");
+                    followedChannels[i].textContent = "⭐" + followedChannels[i].textContent;
+                }
+            }
+
+            appendSettingsButton();
+
+            // Stop refreshing when section is found.
+            window.clearInterval(intervalID);
+        }
+    }
 
     // Creating extension settings button and appending it
     function appendSettingsButton(){
@@ -27,7 +49,7 @@
 
         let text = document.createTextNode("⭐Favourites");
 
-        let node = document.querySelector("#root > div > div.Layout-sc-nxg1ff-0.gnrDvI > nav > div > div.Layout-sc-nxg1ff-0.dmEzmN");
+        let node = document.querySelector("#root > div.Layout-sc-nxg1ff-0.bGJmZt > div.Layout-sc-nxg1ff-0.bSuLAT > nav > div > div.Layout-sc-nxg1ff-0.hWJFll");
 
 
         button.appendChild(text);
@@ -46,19 +68,4 @@
         // Refreshing the page to make sure everything is updated
         window.location.reload(true);
     }
-
-    // Styling favourite channels
-    function updateFavourites(){
-        let followedChannels = document.querySelectorAll("[data-a-target='side-nav-title']");
-        let favouritesStrings = window.localStorage.getItem('favourites').toLowerCase();
-        let favouritesArray = favouritesStrings.split(',');
-
-        for(let i=0; i<followedChannels.length; i++){
-            if(favouritesArray.includes(followedChannels[i].title.toLowerCase())){
-                followedChannels[i].setAttribute("style", "color: gold!important;");
-                followedChannels[i].textContent = "⭐" + followedChannels[i].textContent;
-            }
-        }
-    }
-
 })();
